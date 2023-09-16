8397d8712b7f495e2f5bbdfce0c9f089f46adb5e
shobdeviz <145178183+shobdeviz@users.noreply.github.com>
2023-09-16 09:24:40 +0530

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,11 @@
+var index= 0;
+function changecolors(){
+    var colors= ["red","blue","green"]
+    document.getElementsByTagName("body")[0].
+    style.background = colors[index++]
+    
+    if (index > colors.length -1)
+    index=o;
+
+
+}
